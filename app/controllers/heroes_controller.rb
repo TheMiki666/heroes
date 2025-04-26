1f56d55da2d93c71c79fb8a955e2464b8ab0f0be
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2025-04-26 06:34:32 +0200

    Do heroe#new and heroe#create methods

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -5,4 +5,20 @@ class HeroesController < ApplicationController
   def show
     @heroe=Heroe.find(params[:id])
   end
+  def new
+    @heroe=Heroe.new
+  end
+  def create
+    @heroe=Heroe.new(hero_params)
+    if @heroe.save
+      redirect_to(@heroe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def hero_params
+    params.expect(heroe: [ :name, :alias, :age, :id ])
+  end
 end
--- app/models/heroe.rb
+++ app/models/heroe.rb
@@ -1,2 +1,5 @@
 class Heroe < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :alias, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 15, less_than: 1000 }
 end
