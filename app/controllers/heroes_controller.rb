7dff7f7fd51680da112c14bb473bc23b5fde5035
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2025-04-26 11:34:22 +0200

    Do hero#edit and hero#update methods

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -17,6 +17,20 @@ class HeroesController < ApplicationController
     end
   end
 
+  def edit
+    @hero = Hero.find(params[:id])
+  end
+
+  def update
+    @hero = Hero.find(params[:id])
+
+    if @hero.update(hero_params)
+      redirect_to(@hero)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def hero_params
     params.expect(hero: [ :name, :alias, :age, :id ])
